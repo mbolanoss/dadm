782031c09f76b7e7f2fd1f289f9f2f0ea3afc0b4
mbolanoss <mbolanoss@unal.edu.co>
2023-11-02 12:33:25 -0500

    Added map and user current position

--- reto_9/lib/main.dart
+++ reto_9/lib/main.dart
@@ -1,10 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:provider/provider.dart';
+import 'package:reto_9/services/geolocator_service.dart';
 import 'package:reto_9/utils/custom_theme.dart';
 
 import 'screens/home_screen.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  final geoLocator = GeoLocatorService();
+  final userCoords = await geoLocator.getCurrentPosition();
+
+  runApp(
+    MultiProvider(
+      providers: [
+        Provider<Position>(
+          create: (_) => userCoords,
+        ),
+      ],
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
--- reto_9/lib/screens/home_screen.dart
+++ reto_9/lib/screens/home_screen.dart
@@ -1,10 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:geolocator/geolocator.dart';
+import "package:latlong2/latlong.dart";
+import 'package:provider/provider.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    final userCoords = context.read<Position>();
+
+    return FlutterMap(
+      options: MapOptions(
+        initialZoom: 9.2,
+        initialCenter: LatLng(userCoords.latitude, userCoords.longitude),
+      ),
+      children: [
+        TileLayer(
+          urlTemplate:
+              'https://api.mapbox.com/styles/v1/mbolanoss/clohfm14g001401pee8zi3xaq/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoibWJvbGFub3NzIiwiYSI6ImNsb2c3bXFuMzBxbDYyam8zbGk4cmE1dGQifQ.hI6q-CDuhpk54sNiVjc2Iw',
+          userAgentPackageName: 'com.example.app',
+        ),
+      ],
+    );
   }
 }
